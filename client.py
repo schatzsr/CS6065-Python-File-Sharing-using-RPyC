cc8139c7aea1952dce3255abfed29a18a20e0db7
Anthony Kleiser <kleiseaj@mail.uc.edu>
2015-01-28 17:24:44 -0500

    Basic file sharing functionality has been added.

--- client.py
+++ client.py
@@ -1,51 +1,83 @@
-import sys,os,rpyc
+import sys
+import os
+import rpyc
 
 def showSharedFiles(directory):
   print os.listdir(directory)
 
 def openAndPack(filename):
-    #Open the passed file in read-binary mode
-    #   and write the binary to a string.
+    """
+    Open the passed file in read-binary mode
+    and write the binary to a string.
+    """
     file = open(filename, 'rb')
-    str = file.read()
+    return file.read()
 
 def receiveAndUnpack(binary, filename):
-    #Open File to write to and write input
-    #   binary to the new file
+    """
+    Open File to write to and write input
+    binary to the new file
+    """
     file = open(filename, 'wb')
     file.write(binary)
 
-# Connect to RPyC with input ip, if no input
-#   ip then default to localhost.
-try:
-    c = rpyc.connect(sys.argv[3], 18861)
-except:
-    c = rpyc.connect("localhost", 18861)
-
-# In order to print the messages from others while the client thread
-# is waiting for keyboard input, start up a background listening thread.
-bgsrv = rpyc.BgServingThread(c)
-
-# Set client variables
-#   name          = user's name
-#   shareFolder   = path to folder with shared files
-#   receiveFolder = path to folder to store received files
+'''
+Set client variables
+   name          = user's name
+   shareFolder   = path to folder with shared files
+   receiveFolder = path to folder to store received files
+'''
 name            = raw_input("Enter Your Name:")
 shareFolder     = sys.argv[1]
 receiveFolder   = sys.argv[2]
 firstRun        = True
 
-# Send the function to print a message to my screen to the server
-c.root.setCallback(showSharedFiles)
-
 while True:
 
     if(firstRun):
-        print "Type 'ls' to show available files."
+        # Connect to RPyC with input ip, if no input
+        #   ip then default to localhost.
+        try:
+            c = rpyc.connect(sys.argv[3], 18861)
+        except:
+            c = rpyc.connect("localhost", 18861)
+
+        # In order to print the messages from others while the client thread
+        # is waiting for keyboard input, start up a background listening thread.
+        bgsrv = rpyc.BgServingThread(c)
+
+        # Make files in shareFolder availble to other
+        #   users by adding to the server.
+        fileNames = []
+        fileBins = []
+        for file in os.listdir(shareFolder):
+            fileNames.append(file)
+            fileBins.append(openAndPack(shareFolder + "/" + file))
+        c.root.addUser(name, fileNames, fileBins)
         firstRun = False
-    something = raw_input()
-    if something == "ls":
-        c.root.say(shareFolder)
+
+    # Send the function to print a message to my screen to the server
+    c.root.setCallback(showSharedFiles)
+    userInput = raw_input("Type 'ls' to show available files, '#<userID>:<docID>' to download, or 'q' to quit.\n")
+    if userInput == "ls":
+        display = c.root.showFiles()
+        print display
+    elif userInput[0] == "#":
+        userID = userInput[1]
+        print userID
+        docID = userInput[3]
+        print docID
+        fileInfo = c.root.sendFile(userID, docID)
+        fileInfo[0] = str(fileInfo[0])
+        fileName = receiveFolder + "/" + fileInfo[0]
+        receiveAndUnpack(fileInfo[1], fileName)
+        #selects user
+        #selects file
+        #asks server for file using user/filename
+        #calls recieve and unpack for previous fileName and returned binary
+    elif userInput == "q":
+        #disconnect and remove user from the server
+        print "You quitin' dis shit.....nah"
 
 
 bgsrv.stop()
--- server.py
+++ server.py
@@ -1,44 +1,122 @@
 import rpyc
 
-
 ''' Class to store all external function calls
 '''
 class Service(rpyc.Service):
-  def on_connect(self):
-    pass
+    def on_connect(self):
+        pass
 
-  def on_disconnect(self):
-    print "Someone left!"
+    def on_disconnect(self):
+        print "Someone left!"
 
-  def exposed_setCallback(self,showMe):
-    c.setCallback(showMe)
+    def exposed_setCallback(self,showMe):
+        c.setCallback(showMe)
 
-  def exposed_say(self, shareFolder):
-    c.say(shareFolder)
+    def exposed_say(self, shareFolder):
+        c.say(shareFolder)
+
+    def exposed_addUser(self, name, fileNames, fileBins):
+        c.addUser(name, fileNames, fileBins)
+
+    def exposed_sendFile(self, userID, docID):
+        return c.sendFile(userID, docID)
+
+    def exposed_showFiles(self):
+        return c.showFiles()
 
 ''' A chat server class
   This stores a single state for all the clients
   in the self.callbacks private variable
 '''
-class ChatServer:
-  def __init__(self):
-    self.callbacks = []
+class FileshareServer:
+    def __init__(self):
+        self.callbacks  = []
+        self.users      = []
 
-  # Append the function call (remote) to the list of clients
-  def setCallback(self, showMe):
-    self.callbacks = self.callbacks + [showMe]
+    # Append the function call (remote) to the list of clients
+    def setCallback(self, showMe):
+        self.callbacks = self.callbacks + [showMe]
 
-  # Send that message to everyone's showMe method
-  def say(self, shareFolder):
-    for fn in self.callbacks:
-      try:  # Put in a try/except block just in case we lost net connection
-        fn(shareFolder)
-      except:
-        pass
+    # Send that message to everyone's showMe method
+    def say(self, shareFolder):
+        for fn in self.callbacks:
+            try:  # Put in a try/except block just in case we lost net connection
+                fn(shareFolder)
+            except:
+                pass
+
+    def addUser(self, name, fileNames, fileBins):
+        user = UserFileShare(name)
+        user.setSharedFiles(fileNames, fileBins)
+        self.users.append(user)
+
+    def sendFile(self, userID, docID):
+        print(userID, docID)
+        dindex = 0
+        uIndex = 0
+        print(1)
+        for user in self.users:
+            dindex = 0
+            print(2)
+            print(type(uIndex), type(userID))
+            if uIndex == int(userID):
+                print(3)
+                for file in user.getFileNames():
+                    if dindex == int(docID):
+                        print(4)
+                        rtrn = []
+                        rtrn.append(user.getFileNames()[dindex])
+                        rtrn.append(user.getFileBin(dindex))
+                        print (type(rtrn[0]), type(rtrn[1]))
+                        return rtrn
+                    else:
+                        dindex += 1
+            else:
+                uIndex += 1
+
+    def showFiles(self):
+        ucount = 0
+        rtrn = "User and Files\n"
+        for user in self.users:
+            fcount = 0
+            name = user.getName()
+            rtrn += str(ucount) + ": " + name + "\n"
+            ucount += 1
+            for fileN in user.getFileNames():
+                rtrn += "   "
+                rtrn += str(fcount) + ":" + fileN + "\n"
+                fcount += 1
+        if ucount == 0:
+            return "No users present"
+        else:
+            print rtrn
+            return rtrn
+
+
+class UserFileShare:
+    def __init__(self, name):
+        print name
+        self.name       = name
+        self.fileNames  = []
+        self.fileBins   = []
+        print "User " + name + "'s files have been added."
+
+    def setSharedFiles(self, fileNames, fileBins):
+        self.fileNames  = fileNames
+        self.fileBins   = fileBins
+
+    def getName(self):
+        return self.name
+
+    def getFileNames(self):
+        return self.fileNames
+
+    def getFileBin(self, index):
+        return self.fileBins[index]
 
 if __name__ == "__main__":
   from rpyc.utils.server import ThreadedServer
-  c = ChatServer()
+  c = FileshareServer()
   t = ThreadedServer(Service, port = 18861)
   t.start()
 
